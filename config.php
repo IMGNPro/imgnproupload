ce9331195fc54672599c2ae23870ea84ce1ab452
Johnny <jerh56@gmail.com>
2017-07-23 09:05:23 -0600

    ruta erronea

--- config.php
+++ config.php
@@ -2,4 +2,4 @@
 define('AWS_KEY', 'AKIAJ6JSJIO3U7H6JOUA');
 define('AWS_SECRET', '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');
 define('BUCKET_NAME', 'imgnpro');
-define('BASE_S3_PATH', 'multipartuploads/');
+define('BASE_S3_PATH', '/multipartuploads/');
