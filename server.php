57061e436d36a38c5565bfdd04c6c66618aae626
Johnny <jerh56@gmail.com>
2017-07-23 08:32:41 -0600

    se corrigió error en la firma de archivos

--- server.php
+++ server.php
@@ -14,7 +14,10 @@ switch ($command) {
             $file = BASE_S3_PATH.time().$file;
             $exp = time() + 300;
     		//init upload
-			$upload_sig = base64_encode(hash_hmac("sha1", "POST\n\n\n$exp\n/".BUCKET_NAME.$file."?uploads", AWS_SECRET, true));
+			//$upload_sig = base64_encode(hash_hmac("sha1", "POST\n\n\n$exp\n/".BUCKET_NAME.$file."?uploads", AWS_SECRET, true));
+			
+			$upload_sig = base64_encode(hash_hmac("sha1", "POST\n\n\n$exp\n/".$file."?uploads", AWS_SECRET, true));
+			
 			//$url = "http://s3-ap-southeast-1.amazonaws.com/".BUCKET_NAME.$file."?uploads&".
 			$url = "https://".BUCKET_NAME.".s3.amazonaws.com/".$file."?uploads&".
 			http_build_query(array("Signature"=>$upload_sig, "Expires"=>$exp, "AWSAccessKeyId"=>AWS_KEY));
@@ -35,19 +38,22 @@ switch ($command) {
 		$upId = $_REQUEST['uploadId'];
 		$exp = time() + 7200;
 		for($part=1;$part<=$numParts;$part++) {
-			$upload_sig = base64_encode(hash_hmac("sha1", "PUT\n\n\n$exp\n/".BUCKET_NAME.$file."?partNumber=$part&uploadId=$upId", AWS_SECRET, true));
+			//$upload_sig = base64_encode(hash_hmac("sha1", "PUT\n\n\n$exp\n/".BUCKET_NAME.$file."?partNumber=$part&uploadId=$upId", AWS_SECRET, true));
+			$upload_sig = base64_encode(hash_hmac("sha1", "PUT\n\n\n$exp\n/".$file."?partNumber=$part&uploadId=$upId", AWS_SECRET, true));
 			$url = "https://".BUCKET_NAME.".s3.amazonaws.com/".$file."?". http_build_query(array("partNumber"=>$part, "uploadId"=>$upId, "Signature"=>$upload_sig, "Expires"=>$exp, "AWSAccessKeyId"=>AWS_KEY));
 			//$url = "http://s3-ap-southeast-1.amazonaws.com/".BUCKET_NAME.$file."?". http_build_query(array("partNumber"=>$part, "uploadId"=>$upId, "Signature"=>$upload_sig, "Expires"=>$exp, "AWSAccessKeyId"=>AWS_KEY));
 
 			$parts[] = array("index"=>$part-1, "url"=>$url);
 		}
-		$upload_sig = base64_encode(hash_hmac("sha1", "DELETE\n\n\n$exp\n/".BUCKET_NAME.$file."?uploadId=$upId", AWS_SECRET, true));
+		//$upload_sig = base64_encode(hash_hmac("sha1", "DELETE\n\n\n$exp\n/".BUCKET_NAME.$file."?uploadId=$upId", AWS_SECRET, true));
+		$upload_sig = base64_encode(hash_hmac("sha1", "DELETE\n\n\n$exp\n/".$file."?uploadId=$upId", AWS_SECRET, true));
 
 		//$aborturl = "http://s3-ap-southeast-1.amazonaws.com/".BUCKET_NAME.$file."?". http_build_query(array("uploadId"=>$upId, "Signature"=>$upload_sig, "Expires"=>$exp, "AWSAccessKeyId"=>AWS_KEY));
 		$aborturl = "https://".BUCKET_NAME.".s3.amazonaws.com/".$file."?". http_build_query(array("uploadId"=>$upId, "Signature"=>$upload_sig, "Expires"=>$exp, "AWSAccessKeyId"=>AWS_KEY));
 
 		$exp = time() + 18000;
-		$upload_sig = base64_encode(hash_hmac("sha1", "POST\n\ntext/plain; charset=UTF-8\n$exp\n/".BUCKET_NAME.$file."?uploadId=$upId", AWS_SECRET, true));
+		//$upload_sig = base64_encode(hash_hmac("sha1", "POST\n\ntext/plain; charset=UTF-8\n$exp\n/".BUCKET_NAME.$file."?uploadId=$upId", AWS_SECRET, true));
+		$upload_sig = base64_encode(hash_hmac("sha1", "POST\n\ntext/plain; charset=UTF-8\n$exp\n/".$file."?uploadId=$upId", AWS_SECRET, true));
 
 		//$completeurl = "http://s3-ap-southeast-1.amazonaws.com/".BUCKET_NAME.$file."?". http_build_query(array("uploadId"=>$upId, "Signature"=>$upload_sig, "Expires"=>$exp, "AWSAccessKeyId"=>AWS_KEY));
 		$completeurl = "https://".BUCKET_NAME.".s3.amazonaws.com/".$file."?". http_build_query(array("uploadId"=>$upId, "Signature"=>$upload_sig, "Expires"=>$exp, "AWSAccessKeyId"=>AWS_KEY));
